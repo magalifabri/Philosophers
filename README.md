6b974ccdc1157bc4f590f61616d999b2c5efc8de
Magali Fabri <magali_90@live.nl>
2020-07-17 19:51:43 +0200

    thread creation works

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
+# Philosophers - WORK IN PROGRESS
+
 ## Resources
 - [How to create and join threads in C (pthreads).](https://www.youtube.com/watch?v=uA8X5zNOGw8&list=PL9IEJIKnBJjFZxuqyJ9JqVYmuFZHr7CFM)
 - [How to pass arguments to and get results from threads. (pthread_create, pthread_join)](https://www.youtube.com/watch?v=It0OFCbbTJE&list=PL9IEJIKnBJjFZxuqyJ9JqVYmuFZHr7CFM&index=2)
-- 
+- [Safety and Speed Issues with Threads. (pthreads, mutex, locks)](https://www.youtube.com/watch?v=9axu8CUvOKY&list=PL9IEJIKnBJjFZxuqyJ9JqVYmuFZHr7CFM&index=3)
--- main.c
+++ main.c
@@ -1,32 +1,112 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-void *my_turn(void *arg)
+// typedef struct philosopher_state
+// {
+
+// }
+
+// void *phi_f(void *arg)
+// {
+// 	printf("hello, hello\n");
+// 	return (NULL);
+// }
+
+// int main()
+// {
+// 	int number_of_philosophers;
+// 	int time_to_die;   // time in ms before the next meal needs to start
+// 	int time_to_eat;   // duration in ms that the philo will spend eating
+// 	int time_to_sleep; // duration in ms that the philosopher will spend sleeping
+
+// 	number_of_philosophers = 2;
+// 	time_to_die = 16000;
+// 	time_to_eat = 1000;
+// 	time_to_sleep = 8000; // 8000ms = 1s
+
+// 	// create philosiphers functions
+// 	void (**phi_f)(void *);
+// 	phi_f = malloc(sizeof(phi_f) * number_of_philosophers);
+
+// 	// create philosophers
+// 	char *phi = malloc(number_of_philosophers + 2);
+// 	pthread_t *phi_t = malloc(sizeof(pthread_t) * (number_of_philosophers + 2));
+// 	phi[number_of_philosophers + 1] = '\0';
+// 	int i = 0;
+// 	phi[i] = ' ';
+// 	while (++i <= number_of_philosophers)
+// 	{
+// 		phi[i] = ' ';
+// 		pthread_create(&phi_t[i],
+// 					   NULL,
+// 					   phi_f[i],
+// 					   NULL);
+// 	}
+// }
+
+// void *my_turn(void *arg)
+// {
+// 	while(1)
+// 	{
+// 		sleep(1);
+// 		printf("my turn!\n");
+// 	}
+// 	return (NULL);
+// }
+
+// void your_turn()
+// {
+// 	while(1)
+// 	{
+// 		sleep(2);
+// 		printf("your turn!\n");
+// 	}
+// }
+
+// int main()
+// {
+// 	pthread_t new_thread;
+
+// 	pthread_create(&new_thread, NULL, my_turn, NULL);
+// 	// my_turn();
+// 	your_turn();
+// 	return (0);
+// }
+
+typedef struct s_tab
+{
+	int phi_n;
+	char state;
+} t_tab;
+
+void *phi_f(void *arg)
 {
 	while(1)
 	{
 		sleep(1);
-		printf("my turn!\n");
+		printf("%d\n", (int)arg);
 	}
 	return (NULL);
 }
 
-void your_turn()
-{
-	while(1)
-	{
-		sleep(2);
-		printf("your turn!\n");
-	}
-}
-
+// current state: 3 threads are created and their i index number is passed to phi_f.
 int main()
 {
-	pthread_t new_thread;
+	// create threads
+	int number_of_philosophers = 3;
+	pthread_t *phi_t;
+	phi_t = malloc(sizeof(pthread_t) * number_of_philosophers + 1);
+	phi_t[number_of_philosophers] = NULL;
 
-	pthread_create(&new_thread, NULL, my_turn, NULL);
-	// my_turn();
-	your_turn();
+	void *i = 0;
+	while ((int)i < number_of_philosophers)
+	{
+		pthread_create(&phi_t[(int)i], NULL, phi_f, i);
+		i++;
+	}
+
+	pthread_join(*phi_t, NULL);
 	return (0);
 }
