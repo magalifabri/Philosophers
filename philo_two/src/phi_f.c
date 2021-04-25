dbec2218f17cb3e68c54e6144a1bfd5960166388
Magali Fabri <magali@Magalis-MacBook-Pro.local>
2021-04-25 07:14:55 +0200

    changed how s.phi_n is set, now uses a semaphore; added grim_reaper thread to kill threads waiting on a semaphore

--- philo_two/src/phi_f.c
+++ philo_two/src/phi_f.c
@@ -1,6 +1,6 @@
 #include "../philo_two.h"
 
-static void	*starving(t_tab *tab, t_thread_var_struct *s)
+void		*starving(t_tab *tab, t_thread_var_struct *s)
 {
 	if (sem_wait(tab->starving_sem) == -1)
 		return (set_error_code(tab, ERROR_SEM_WAIT));
@@ -8,20 +8,6 @@ static void	*starving(t_tab *tab, t_thread_var_struct *s)
 	printf("%lld %d "B_RED"died"RESET"\n",
 		(tab->current_time - tab->start_time), s->phi_n + 1);
 	return (NULL);
-}
-
-static int	sated(t_tab *tab, t_thread_var_struct *s)
-{
-	if (tab->number_of_times_each_philosopher_must_eat != -1
-		&& tab->n_times_eaten && tab->n_times_eaten[s->phi_n]
-		>= tab->number_of_times_each_philosopher_must_eat)
-	{
-		put_status_msg(tab, s, B_GREEN"is fat enough"RESET);
-		if (sem_post(tab->fork_availability) == -1)
-			return ((int)set_error_code(tab, ERROR_SEM_POST));
-		return (0);
-	}
-	return (1);
 }
 
 static int	eating(t_tab *tab, t_thread_var_struct *s)
@@ -42,7 +28,11 @@ static int	eating(t_tab *tab, t_thread_var_struct *s)
 			return ((int)set_error_code(tab, ERROR_USLEEP));
 	if (tab->n_times_eaten)
 		tab->n_times_eaten[s->phi_n]++;
-	return (sated(tab, s));
+	if (tab->number_of_times_each_philosopher_must_eat != -1
+		&& tab->n_times_eaten && tab->n_times_eaten[s->phi_n]
+		>= tab->number_of_times_each_philosopher_must_eat)
+		put_status_msg(tab, s, B_GREEN"is fat"RESET);
+	return (1);
 }
 
 static int	sleeping(t_tab *tab, t_thread_var_struct *s)
@@ -60,25 +50,31 @@ static int	sleeping(t_tab *tab, t_thread_var_struct *s)
 		if (usleep(1000) == -1)
 			return ((int)set_error_code(tab, ERROR_USLEEP));
 	}
+	put_status_msg(tab, s, "is thinking");
 	return (1);
 }
 
 void	*phi_f(void *arg)
 {
 	t_tab				*tab;
-	t_thread_var_struct	s;
+	t_thread_var_struct	*s;
+	pthread_t			grimreaper_thread;
 
 	tab = (t_tab *)arg;
-	s.time_last_meal = tab->current_time;
-	s.phi_n = tab->phi_n;
+	s = malloc(sizeof(t_thread_var_struct));
+	if (!s)
+		return (set_error_code(tab, ERROR_MALLOC));
+	if (sem_wait(tab->id_sem) == -1)
+		return (set_error_code(tab, ERROR_SEM_WAIT));
+	s->phi_n = tab->phi_n_c++;
+	if (sem_post(tab->id_sem) == -1)
+		return (set_error_code(tab, ERROR_SEM_POST));
+	s->time_last_meal = tab->current_time;
+	s->tab = tab;
+	if (pthread_create(&grimreaper_thread, NULL, grimreaper, s) != 0)
+		return (NULL);
+	pthread_detach(grimreaper_thread);
 	while (1)
-	{
-		if (s.time_last_meal + tab->time_to_die <= tab->current_time)
-			return (starving(tab, &s));
-		if (!eating(tab, &s))
+		if (!eating(tab, s) || !sleeping(tab, s))
 			return (NULL);
-		if (!sleeping(tab, &s))
-			return (NULL);
-		put_status_msg(tab, &s, "is thinking");
-	}
 }
